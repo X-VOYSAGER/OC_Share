05d3dc78010a3c51fcc2d35239b2df7fdd4424f3
X-VOYSAGER <pjsandrade08@gmail.com>
2023-07-14 12:27:57 -0300

    OC Share V1.1

---
+++ config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCrPgidlwxzg20XOXH-8Ux5_tQEwRdIuZw",
+    authDomain: "oc-share-b5027.firebaseapp.com",
+    databaseURL: "https://oc-share-b5027-default-rtdb.firebaseio.com",
+    projectId: "oc-share-b5027",
+    storageBucket: "oc-share-b5027.appspot.com",
+    messagingSenderId: "1036101664275",
+    appId: "1:1036101664275:web:b84101ed1a9f896c31c9e6"
+  };
+  
