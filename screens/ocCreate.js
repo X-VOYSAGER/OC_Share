e3af6df45634ca516d22f762d9bcb0207598a4d6
X-VOYSAGER <pjsandrade08@gmail.com>
2023-08-09 18:25:01 -0300

    oc share att

--- screens/ocCreate.js
+++ screens/ocCreate.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Text, View, StyleSheet, TextInput } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 export default class OcCreate extends React.Component {
     render() {
@@ -40,21 +41,28 @@ export default class OcCreate extends React.Component {
                     </View>
                 </View>
                 <View style={styles.lowerContainer}>
-                    <View style={styles.fieldContainer}>
-                        <Text style={styles.fieldText}>
+                    <View style={[styles.fieldContainer, { height: "100%" }]}>
+                        <Text style={[styles.fieldText, { width: 60 }]}>
                             Extra:
                         </Text>
-                        <TextInput style={styles.textInput}
-                            placeholder="Extra:"
-                            multiline= {true}
+                        <TextInput style={[styles.textInput, { width: "83.6%" }]}
+                            multiline={true}
                             numberOfLines={10}
+                            placeholder="Extra"
                         />
                     </View>
                 </View>
                 <View style={styles.imageContainer}>
                     <Text>
-                        Não sei o que escrever aqui
+                        Imagem
                     </Text>
+                </View>
+                <View style={styles.buttonContainer}>
+                    <TouchableOpacity style={styles.button}>
+                        <Text style={styles.buttonText}>
+                            Publicar
+                        </Text>
+                    </TouchableOpacity>
                 </View>
             </View>
 
@@ -66,7 +74,8 @@ export default class OcCreate extends React.Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: "#ded5d5"
+        backgroundColor: "#ded5d5",
+        marginBottom: 10
     },
     upperContainer: {
         flex: 0.25,
@@ -74,14 +83,17 @@ const styles = StyleSheet.create({
         justifyContent: "space-between",
         borderBottomWidth: 5,
         borderLeftWidth: 5,
-        alignItems: "flex-start"
+        alignItems: "flex-start",
+        padding: 5
     },
     lowerContainer: {
         flex: 0.33,
         //backgroundColor: "green",
         borderBottomWidth: 5,
         borderLeftWidth: 5,
-
+        justifyContent: "space-between",
+        alignItems: "flex-start",
+        padding: 5
 
     },
     imageContainer: {
@@ -89,21 +101,55 @@ const styles = StyleSheet.create({
         //backgroundColor: "blue",
         borderBottomWidth: 5,
         borderLeftWidth: 5,
+        padding: 5
 
     },
     textInput: {
-
-
-
+        //backgroundColor: "blue",
+        width: "75%",
+        borderWidth: 1,
+        textAlign: "left",
+        textAlignVertical: "top",
+        padding: 5,
+        paddingLeft: 8
     },
     fieldText: {
         width: 100,
         fontSize: 22
-
     },
     fieldContainer: {
         flexDirection: "row",
         //justifyContent: "space-evenly",
-        width: "80%"
+        width: "100%",
+        //backgroundColor: "blue",
     },
+    buttonContainer: {
+        flex: 0.15,
+        //backgroundColor: "blue",
+        borderBottomWidth: 5,
+        borderLeftWidth: 5,
+        padding: 5,
+        justifyContent: "flex-start",
+        alignItems: "center"
+
+    },
+    button: {
+        justifyContent: "center",
+        alignItems: "center",
+        borderWidth: 1,
+        backgroundColor: "#880b11",
+        width: 100,
+        padding: 5,
+        borderRadius: 50
+    },
+
+    buttonText: {
+        color: "#FFFFFF"
+
+
+    },
+
+
+
 })
+// exp://eyjtmta.anonymous.19000.exp.direct:80
